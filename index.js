32d9a9bee3de939e4abf125019b903a2339792eb
Paul <pmacellaro7@aol.com>
2023-04-26 17:05:06 -0400

    UwU

---
+++ index.js
@@ -0,0 +1,13 @@
+function add(a,b) { return a+b;}
+function subtract (a,b) {return a-b;}
+function multiply (a,b) {return a*b;}
+function divide (a,b) {return a/b;}
+let n=5
+function increment (n) {return n + 1;}
+function decrement (n) {return n - 1;}
+ let bignumber=69
+function makeInt(bignumber){return Number.parseInt(bignumber,10)}
+ parseInt("2.2222", 10);
+ parseInt("nonsense!", 10);
+ let BIGGERNUMBER= 69.6969696969696969
+function preserveDecimal(BIGGERNUMBER){return parseFloat(BIGGERNUMBER)}
